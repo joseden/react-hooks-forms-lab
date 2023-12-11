a9aa6647080da99a09aa5daa66e01e5ace932161
Joseph <joseph.dena@student.moringaschool.com>
2023-12-11 13:20:18 +0300

    Complete

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,8 +3,14 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
+
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("");
+
+  function onSearchChange(event) {
+    setSearch(event.target.value);
+  }
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
@@ -17,11 +23,26 @@ function ShoppingList({ items }) {
   });
 
   return (
+
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
+      <Filter onCategoryChange={handleCategoryChange}
+        search={search}
+        setSearch={setSearch}
+        onSearchChange={onSearchChange}
+      />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        
+        {itemsToDisplay.filter((item) => {
+          if (search === "") {
+            return true;
+          } else if (item.name.toLowerCase().includes(search.toLowerCase())) {
+            return true;
+          } else {
+            return false;
+          }
+        }).map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
